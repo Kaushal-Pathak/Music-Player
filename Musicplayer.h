1c85c3720068499f0b3c69778bd1ed2c749e709b
Kaushal-Pathak <132580286+Kaushal-Pathak@users.noreply.github.com>
2025-04-30 19:36:31 +0530

    Create Musicplayer.h

---
+++ Musicplayer.h
@@ -0,0 +1,31 @@
+#ifndef MUSICPLAYER_H
+#define MUSICPLAYER_H
+
+#include <QWidget>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QListWidget>
+#include <QFileDialog>
+#include <QMediaPlayer>
+
+class MusicPlayer : public QWidget {
+    Q_OBJECT
+
+public:
+    MusicPlayer(QWidget *parent = nullptr);
+    ~MusicPlayer();
+
+private slots:
+    void addSong();
+    void playSong();
+    void stopSong();
+
+private:
+    QMediaPlayer *mediaPlayer;
+    QListWidget *songList;
+    QPushButton *addButton;
+    QPushButton *playButton;
+    QPushButton *stopButton;
+};
+
+#endif // MUSICPLAYER_H
